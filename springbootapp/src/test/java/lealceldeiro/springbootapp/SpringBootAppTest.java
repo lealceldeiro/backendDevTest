0c1b54a430e07046bdcc0fd4abd9969a9a4fb83b
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-12-25 23:51:49 +0200

    Add util files + test

---
+++ springbootapp/src/test/java/lealceldeiro/springbootapp/SpringBootAppTest.java
@@ -0,0 +1,11 @@
+package lealceldeiro.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppTest {
+  @Test
+  void contextLoads() {
+  }
+}
