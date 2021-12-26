35e5b9f3a520d7a217fb734452acd7ca9f1c3def
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-12-26 11:31:58 +0200

    Update ProductDto
    - Specify custom toString, equals and hashCode implementation

--- springbootapp/src/main/java/lealceldeiro/springbootapp/product/ProductDto.java
+++ springbootapp/src/main/java/lealceldeiro/springbootapp/product/ProductDto.java
@@ -1,12 +1,17 @@
 package lealceldeiro.springbootapp.product;
 
 import java.math.BigDecimal;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ProductDto {
+  @EqualsAndHashCode.Include
   private int id;
   private String name;
   private BigDecimal price;
