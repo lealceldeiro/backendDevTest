b140be51fbe40331ed4719bf47076fa0d6a51fc6
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-12-26 11:34:52 +0200

    Update webclient configuration
    - Update application.yml properties
    - Update WebConfig and WebClient bean provided there

--- springbootapp/src/main/java/lealceldeiro/springbootapp/config/WebConfig.java
+++ springbootapp/src/main/java/lealceldeiro/springbootapp/config/WebConfig.java
@@ -1,39 +1,36 @@
 package lealceldeiro.springbootapp.config;
 
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
+import io.netty.handler.logging.LogLevel;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
+@Slf4j
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "web")
 public class WebConfig {
-  private static final int DEFAULT_TIMEOUT = 240000;
+  private static final int DEFAULT_TIMEOUT = 15000;
 
   private int connectionTimeout = DEFAULT_TIMEOUT;
-  private int readTimeout = DEFAULT_TIMEOUT;
-  private int writeTimeout = DEFAULT_TIMEOUT;
 
   @Bean
   public WebClient webClient() {
-    ChannelHandler readHandler = new ReadTimeoutHandler(readTimeout, TimeUnit.MILLISECONDS);
-    ChannelHandler writeHandler = new WriteTimeoutHandler(writeTimeout, TimeUnit.MILLISECONDS);
+    Duration responseTimeoutDuration = Duration.ofMillis(connectionTimeout);
+    log.info("Setting web client timeout: {}", responseTimeoutDuration);
+
     var httpClient = HttpClient.create()
                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionTimeout)
-                               .responseTimeout(Duration.ofMillis(connectionTimeout))
-        // FIXME: configure properly the timeouts
-                               /*.doOnConnected(conn -> conn.addHandlerLast(readHandler)
-                                                          .addHandlerLast(writeHandler))*/;
+                               .responseTimeout(responseTimeoutDuration)
+                               .wiretap("logger", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
 
     return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).build();
   }
