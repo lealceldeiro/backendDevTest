9ae17e8268adb6351ded7ca321d754b2e5b0b6e0
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-12-26 22:59:35 +0200

    Update ProductServiceImpl
    - Map to Product Not found error if there's any error while fetching similar product ids
    - Add test to cover not found case

--- springbootapp/src/main/java/lealceldeiro/springbootapp/product/ProductServiceImpl.java
+++ springbootapp/src/main/java/lealceldeiro/springbootapp/product/ProductServiceImpl.java
@@ -3,11 +3,8 @@ package lealceldeiro.springbootapp.product;
 import lealceldeiro.springbootapp.config.AppConf;
 import lealceldeiro.springbootapp.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.reactivestreams.Publisher;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -39,16 +36,11 @@ public class ProductServiceImpl implements ProductService {
                     .retrieve()
                     .bodyToMono(Integer[].class)
                     .flatMapMany(Flux::fromArray)
-                    .onErrorResume(this::handleNotFoundException);
-  }
-
-  private <T> Publisher<? extends T> handleNotFoundException(Throwable e) {
-    log.error("Error while fetch similar ids", e);
-
-    boolean isNotFound = WebClientResponseException.class.isAssignableFrom(e.getClass()) &&
-                         HttpStatus.NOT_FOUND == ((WebClientResponseException) e).getStatusCode();
-
-    return isNotFound ? Mono.error(new NotFoundException()) : Flux.empty();
+                    .onErrorResume(e -> {
+                      log.error("Error while fetching similar ids for product ({}). It'll be mapped"
+                                + " to a 'Not Found product'", productId, e);
+                      return Mono.error(new NotFoundException());
+                    });
   }
 
   private Flux<ProductDto> getProductDetails(Flux<Integer> productIds) {
---
+++ springbootapp/src/test/java/lealceldeiro/springbootapp/product/ProductServiceImplComponentTest.java
@@ -0,0 +1,19 @@
+package lealceldeiro.springbootapp.product;
+
+import lealceldeiro.springbootapp.exception.NotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceImplComponentTest {
+  @Autowired
+  private ProductService productService;
+
+  @Test
+  void getSimilarProductsReturnsNotFound() {
+    var flux = productService.getSimilarProducts(Integer.MAX_VALUE);
+    Assertions.assertThrows(NotFoundException.class, flux::blockFirst);
+  }
+}
